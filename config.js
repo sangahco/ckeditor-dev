612b2dfcdce0f2db9f4a7546b0bae0b24be78c01
Emanuele Disco <emawind84@gmail.com>
2016-08-19 21:37:45 +0900

    update

--- config.js
+++ config.js
@@ -42,8 +42,13 @@ CKEDITOR.editorConfig = function( config ) {
 	//config.fullPage = true;
 	config.removePlugins = 'base64image';
 	config.removeButtons = 'Image';
-	config.font_names = "맑은 고딕/맑은 고딕, sans-serif;굴림/굴림, sans-serif; HY견고딕/HY견고딕, sans-serif;" + config.font_names;
+	config.font_names = "맑은 고딕/Malgun Gothic,'맑은 고딕',sans-serif;돋움/돋움,Dotum,sans-serif;굴림/Gulim,굴림,AppleGothic,sans-serif;HY견고딕/HY견고딕,sans-serif;" + config.font_names;
 	config.baseFloatZIndex = 120;
+	
+	// copyfromword options
+	config.pasteFromWordRemoveFontStyles = false;
+	config.pasteFromWordRemoveStyles = false;
+	//config.pasteFromWordPromptCleanup = true;
 
 	CKEDITOR.dtd.$removeEmpty.span = 0;
 	
