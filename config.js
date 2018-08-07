aef965d354de59d13ee43941b09a37747c9a4c3f
Emanuele Disco <emawind84@gmail.com>
2018-08-07 10:51:05 +0900

    remove meta tag from disallowed list

--- config.js
+++ config.js
@@ -84,7 +84,7 @@ CKEDITOR.editorConfig = function( config ) {
 	        classes: true
 	    }
 	};
-	config.disallowedContent = 'script;*{mso-*,-ms-*};*(mso-*);title;meta;link;';
+	config.disallowedContent = 'script;*{mso-*,-ms-*};*(mso-*);title;link;';
 
 	if(typeof __LOCALE__ !== "undefined"){
 		config.language = __LOCALE__ || 'en';
